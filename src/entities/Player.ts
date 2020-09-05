98be9a9d046ba28459c57ceb5eb3125c2506d8d8
Ayfri <pierre.ayfri@gmail.com>
2020-09-05 23:33:38 +0200

    Updated dependencies, removed unused dependencies.

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,17 +1,36 @@
 import Block from '../blocks/Block';
 import {game} from '../main';
-import {resolution} from '../ressources/GameData';
+import Position from '../utils/Position';
 import TilePosition from '../utils/TilePosition';
+
 
 export default class Player {
 	public blockSelected: Block;
+	public position: Position;
+	public velocity: Position;
+	public sprite: PIXI.Texture;
 	
 	constructor() {
 		this.blockSelected = game.gameData.blocks.get('dirt');
+		this.position = new Position();
+		this.velocity = new Position();
+	}
+	
+	public move(x: number, y: number): void {
+		this.velocity.x = x;
+		this.velocity.y = y;
+	}
+	
+	public update(): void {
+		this.updatePosition();
 	}
 	
 	public putBlockWhereClicked(): void {
 		const position: TilePosition = TilePosition.fromPositionToShortTilePosition(game.mouseManager.getMousePosition());
 		game.world.replaceBlock(this.blockSelected, position);
+	}
+	
+	private updatePosition(): void {
+		this.position.add(this.velocity.x, this.velocity.y);
 	}
 }
