555bf856ad841c27ae024b576cfccf82d87f9eba
Ayfri <pierre.ayfri@gmail.com>
2020-09-10 21:21:14 +0200

    Added FPS counter.

--- src/client/DebugGui.ts
+++ src/client/DebugGui.ts
@@ -5,16 +5,21 @@ import * as PIXI from 'pixi.js';
 
 export default class DebugGui extends Gui {
 	public mouseCoordsText: PIXI.Text;
+	public fpsCounter: PIXI.Text;
 
 	public constructor(public app: PIXI.Application) {
 		super(app);
 		this.mouseCoordsText = new PIXI.Text('', new PIXI.TextStyle({ fill: '#ffffff' }));
+		this.fpsCounter = new PIXI.Text('', new PIXI.TextStyle({ fill: '#ffffff' }));
 		this.mouseCoordsText.position.set(window.innerWidth - 200, 50);
+		this.fpsCounter.position.set(window.innerWidth - 200, 20);
 		this.addPIXISprite('mouseCoordsText', this.mouseCoordsText);
+		this.addPIXISprite('fpsCounter', this.fpsCounter);
 	}
 
 	public update(): void {
 		const mousePosition = TilePosition.fromPositionToShortTilePosition(game.mouseManager.getMousePosition());
 		this.mouseCoordsText.text = `x: ${mousePosition.x}, y: ${mousePosition.y}`;
+		this.fpsCounter.text = `FPS: ${game.app.ticker.FPS.toFixed()}`;
 	}
 }
