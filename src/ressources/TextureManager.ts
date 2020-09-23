960373f41c3b99c915c5e1f15aa0aac25929f586
Ayfri <pierre.ayfri@gmail.com>
2020-09-23 22:57:37 +0200

    Added saved textures in TextureManager.

--- src/ressources/TextureManager.ts
+++ src/ressources/TextureManager.ts
@@ -1,8 +1,12 @@
 import PIXI from '../PIXI';
 import { Path } from '../types';
+import Collection from '../utils/Collection';
 
 export default class TextureManager {
+	private readonly savedTextures: Collection<string, PIXI.Texture>;
+
 	public constructor(public app: PIXI.Application) {
+		this.savedTextures = new Collection();
 		this.preLoadTexture('./assets/sprites/background.png', 'background');
 	}
 
@@ -10,7 +14,13 @@ export default class TextureManager {
 		this.app.loader.add(name, path);
 	}
 
-	public getTexture(name: string): PIXI.Texture {
-		return this.app.loader.resources[name]?.texture;
+	public getTexture(name: string): PIXI.Texture | undefined {
+		const texture: PIXI.Texture | undefined = this.app.loader.resources[name]?.texture;
+		if (texture && !this.savedTextures.has(name)) this.savedTextures.set(name, texture);
+		return texture;
+	}
+
+	public getTextures(): Collection<string, PIXI.Texture> {
+		return this.savedTextures;
 	}
 }
